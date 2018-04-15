c2c134db054be5083d4889ca3082429a8923bfe2
dazdaz <darren@dazdaz.org>
2018-04-15 09:52:14 +0800

    Update azure_aci.sh

--- azure_aci.sh
+++ azure_aci.sh
@@ -1,12 +1,10 @@
-## Deploy a single container instance using the Azure Container Instance (ACI) Service.
+# Deploy a single container instance using the Azure Container Instance (ACI) Service.
 
-```
 $ az container create -g acidemo --name nginx --image microsoft/aci-helloworld --os-type linux --cpu 1 --memory 1 \
   --ip-address public --ports 80 443 --dns-name-label acidemo
-```
 
-* Goto http://acidemo.southeastasia.azurecontainer.io//
+echo "http://acidemo.southeastasia.azurecontainer.io/"
 
-## Updates
-https://azure.microsoft.com/en-us/services/container-instances/
-https://azure.microsoft.com/en-us/updates/aci-feb/
+# More onfo :
+# https://azure.microsoft.com/en-us/services/container-instances/
+# https://azure.microsoft.com/en-us/updates/aci-feb/
